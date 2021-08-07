2c51963637b0ff92c030de8d12b4ffbdd7dff1aa
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-07 11:54:28 -0400

    Add DataTables source files to public and removed CDN link

--- src/pages/StarMapDB/index.jsx
+++ src/pages/StarMapDB/index.jsx
@@ -12,12 +12,10 @@ export default function Home() {
         <title>{title}</title>
         <meta name="description" content={title} />
         <link rel="icon" href="/favicon.ico" />
-        {/* <link rel="stylesheet" href="/api/css/datatables" /> */}
-        {/* <script src="/api/scripts/datatables"></script> */}
         <link rel="stylesheet" type="text/css"
-            href="https://cdn.datatables.net/v/bs5/dt-1.10.25/sl-1.3.3/datatables.css"/>
+            href="/datatables/datatables.min.css"/>
         <script type="text/javascript"
-            src="https://cdn.datatables.net/v/bs5/dt-1.10.25/sl-1.3.3/datatables.js"></script>
+            src="/datatables/datatables.min.js"></script>
     </Head>
     <App/>
     </>)
