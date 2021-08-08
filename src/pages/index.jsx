961b7c145b802b30a0d1b963cb463fcc6394395d
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-08 19:46:04 -0400

    Added a temp welcome message to my homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,6 +10,7 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <h1>Justin Hansen's Portfolio</h1>
+        <p>Hello, welcome to my site. This is currently a work in progress to show a few applications that I have made.</p>
         <Link href="/Lets-Roll">
             <a>Roll Probability Calculator</a>
         </Link>
