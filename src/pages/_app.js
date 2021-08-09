144c7ed342a739ba2d8fe66fe41e3dc8e1724b1c
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-08 22:34:18 -0400

    Quick fix to include bootstrap js so the collapsed navbar menu works

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <script type="text/javascript" src="/jquery/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="/bootstrap/js/bootstrap.bundle.min.js"></script>
   </Head>
 
   <Component {...pageProps}/>
