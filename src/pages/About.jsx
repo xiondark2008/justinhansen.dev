4709fe662782287096752be35636c9dfc2e0e60c
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-13 17:52:31 -0400

    Built out the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,50 +1,69 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import Navbar from '@/portfolio/components/Navbar'
+import Head from 'next/head';
+import Link from 'next/link';
+import Navbar from '@/portfolio/components/Navbar';
+
+import style from '@/portfolio/styles/About.module.scss';
 
 export default function About() {
+    const breakpoint = 'lg',
+        headshotSmallClassName = ([style.headshot, style['headshot-small'], 'd-'+breakpoint+'-none']).join(' '),
+        headshotBigClassName = ([style.headshot, 'd-none d-'+breakpoint+'-block col col-'+breakpoint+'-4']).join(' '),
+        headshot = ( className ) => {
+            return(
+            <figure className={ className }>
+                <img className="img-fluid"
+                    src="/images/gray-grid.png"
+                    alt="headshot"
+                />
+            </figure>
+            )
+        }
+
     return (<>
     <Head>
         <title>Justin Hansen</title>
         <meta name="description" content="Justin Hansen's portfolio" />
         <link rel="icon" href="/favicon.ico" />
     </Head>
-    <Navbar currentPage={ 'About' }/>
-    <div className="container-fluid">
-        <div className="row justify-content-evenly">
-            <section className="col col-md-6">
-                <h6>//HELLO</h6>
-                <h1>About Justin</h1>
-                <p>I’m a Front-End Developer with over 8+ years of experience developing clean 
-                user experiences.</p>
-                <p>My road to development started like every great creator of our time - in my 
-                college dorm room. My freshman friend was really into computers so much so that 
-                he had built his own. One afternoon he decided to take it apart to rebuild it 
-                cleaner, more efficiently. Being a life long tinkerer and genuinely curious I was 
-                intrigued so I joined him.</p>
-                <p>Quickly I was hooked. I was fascinated with how the peaces fit together - worked 
-                together - to make something bigger. But that was just the start. Now that I could 
-                build a machine, could I tell it how to work?</p>
-                <p>So I taught myself to code.</p>
-                
-                <h4>Technical Skills</h4>
-                <ul id="tech-skills" className="d-flex flex-column flex-wrap list-unstyled"
-                    style={ {height: '7rem', width: '15rem'} }
-                >
-                    <li>JavaScript</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>Java</li>
-                    <li>React.js</li>
-                    <li>Sass</li>
-                    <li>Node.js</li>
-                    <li>SQL</li>
-                </ul>
-                <a className="btn btn-primary">My Resume</a>
-            </section>
-            <figure className="col col-md-4">
-                <div>headshot</div>
-            </figure>
+    <div className={ style.body }>
+        <Navbar currentPage={ 'About' }/>
+        <div className={ "container-fluid py-3 pt-"+breakpoint+"-5" }>
+            <div className="row justify-content-evenly">
+                <section className={ "col-12 col-"+breakpoint+"-6" }>
+                    <h5 className="code-comment">//HELLO</h5>
+                    <h1 className="display-4 font-bk">ABOUT ME</h1>
+                    { headshot( headshotSmallClassName ) }
+                    <p>I’m a Front-End Developer with over 8+ years of experience developing clean 
+                    user experiences.</p>
+                    <p>My road to development started like every great creator of our time - in my 
+                    college dorm room. My freshman friend was really into computers so much so that 
+                    he had built his own. One afternoon he decided to take it apart to rebuild it 
+                    cleaner, more efficiently. Being a life long tinkerer and genuinely curious I was 
+                    intrigued so I joined him.</p>
+                    <p>Quickly I was hooked. I was fascinated with how the peaces fit together - worked 
+                    together - to make something bigger. But that was just the start. Now that I could 
+                    build a machine, could I tell it how to work?</p>
+                    <p>So I taught myself to code.</p>
+                    
+                    <h4 className="mt-4 text-primary font-bk">TECHNICAL SKILLS</h4>
+                    <ul id={ style['tech-skills'] }
+                        className="d-flex flex-column flex-wrap align-content-start list-unstyled"
+                    >
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>Java</li>
+                        <li>React.js</li>
+                        <li>Sass</li>
+                        <li>Node.js</li>
+                        <li>SQL</li>
+                    </ul>
+                    <div className={ 'd-grid gap-2 d-'+breakpoint+'-block' }>
+                        <a className='btn btn-primary btn-lg'>My Resume</a>
+                    </div>
+                </section>
+                { headshot( headshotBigClassName ) }
+            </div>
         </div>
     </div>
     </>)
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -21,7 +21,7 @@ export default function Home() {
     {/* feature project roll Probability */}
     <FeaturedWorkLayout>
         <section className="col-12 col-sm-6 col-md-5 col-lg-4 my-5">
-            <h6 className="text-primary">//My Work</h6>
+            <h6 className="code-comment">//My Work</h6>
             <h1>Roll Probability Calculator</h1>
             <p>stuff and things about the app</p>
             <Link href="/Lets-Roll">
@@ -40,7 +40,7 @@ export default function Home() {
             <div>browser screen shot</div>
         </section>
         <section className="col-12 col-sm-6 col-md-5 col-lg-4 my-5">
-            <h6 className="text-primary">//My Work</h6>
+            <h6 className="code-comment">//My Work</h6>
             <h1>StarMap DB</h1>
             <p>stuff and things about the app</p>
             <Link href="/StarMapDB">
