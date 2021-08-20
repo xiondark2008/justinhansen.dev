53a2406b50cd2e803221cb2860824f3f6cd627f4
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-19 23:31:33 -0400

    switched assets to use non-minified versions for dev

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,8 @@ function MyApp({ Component, pageProps }) {
   return(<>
   <Head>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script type="text/javascript" src="/jquery/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="/jquery/jquery-3.6.0.js"></script>
+    <script type="text/javascript" src="/bootstrap/js/bootstrap.bundle.js"></script>
   </Head>
 
   <Component {...pageProps}/>
