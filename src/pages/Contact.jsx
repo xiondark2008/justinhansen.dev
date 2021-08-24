6b27f8c5514f8aa526216fc6ea04af09f5b2e181
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-24 12:05:50 -0400

    filled in Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,40 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import LandingSpace from '@/portfolio/components/layouts/LandingSpace'
+import Head from 'next/head';
+import Link from 'next/link';
+import LandingSpace from '@/portfolio/components/layouts/LandingSpace';
+import { addClassNames } from '@/common/utils/Utilities';
+
+import style from '@/portfolio/styles/Contact.module.scss'
 
 export default function Home() {
+    const contacts = [
+        {
+            label: 'Email',
+            value: <a href='mailto:xiondark2008@gmail.com'
+                      target='_blank'
+                    >xiondark2008@gmail.com</a>
+        },{
+            label: 'LinkedIn',
+            value: <a href='https://www.linkedin.com/in/justin-hansen-26a44546'
+                      target='_blank'
+                    >Go To</a>
+        },{
+            label: 'Stack Overflow',
+            value: <a href='https://stackoverflow.com/users/1824363/xion-dark'
+                      target='_blank'
+                    >Go To</a>
+        },{
+            label: 'JSFiddle',
+            value: <a href='https://jsfiddle.net/user/xiondark2008/fiddles/'
+                      target='_blank'
+                    >Go To</a>
+        },{
+            label: 'GitHub',
+            value: <a href='https://github.com/xiondark2008'
+                      target='_blank'
+                    >Go To</a>
+        }
+    ]
+
     return (<>
     <Head>
         <title>Justin Hansen</title>
@@ -12,7 +44,19 @@ export default function Home() {
     <LandingSpace theme='dark'
         navbarProps={ {currentPage: 'Contact'} }
     >
-        <p>Contact Info</p>
+        <main className='container'>
+            { contacts.map( contact =>
+                <div className={ addClassNames( style.contactLine, 'row') }>
+                    <div className='col-auto'>
+                        { contact.label }
+                    </div>
+                    <div className='col'></div>
+                    <div className='col-auto'>
+                        { contact.value }
+                    </div>
+                </div>
+            ) }
+        </main>
     </LandingSpace>
     </>)
 }
