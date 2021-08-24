52c74d0f9dc605177fe3b0e0f187e1a228a7c905
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-24 09:07:42 -0400

    Added a footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,6 +11,14 @@ function MyApp({ Component, pageProps }) {
   </Head>
 
   <Component {...pageProps}/>
+  <footer className='d-flex align-items-center'>
+    <a className='text-primary font-bk lh-1 text-decoration-none p-3'
+      href='/'
+    ><b>JUSTIN<br/>HANSEN</b></a>
+    <a className='ms-auto text-muted p-3'
+      href='/Contact'
+    >Contact</a>
+  </footer>
   </>)
 }
 
