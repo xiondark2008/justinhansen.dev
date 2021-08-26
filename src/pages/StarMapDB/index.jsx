5782922314ef11c992ab0d83ccb51b1f343a29a8
HANSENJ1@legal.regn.net <justin.hansen@lexisnexis.com>
2021-08-26 02:50:40 -0400

    moved component into index page

--- src/pages/StarMapDB/index.jsx
+++ src/pages/StarMapDB/index.jsx
@@ -1,23 +1,99 @@
 import Head from 'next/head'
-import App from "@/starmap_db/components/StarMapDB.jsx";
+//import App from "@/starmap_db/components/StarMapDB.jsx";
 import Footer from '@/portfolio/components/Footer';
+
+import { Component } from "react";
+import Navbar from "@/starmap_db/components/Navbar.jsx";
+import ListView from "@/starmap_db/components/ListView.jsx";
+import DetailsView from "@/modules/starmap_db/components/DetailsView.jsx";
+import { ENTITY_PATHS, UI_ENTITY_STUBS } from "@/starmap_db/utils/Utilities";
 
 //import "@/starmap_db/styles/StarMapDB.module.scss"
 
-export default function Home() { //console.debug("DEBUG - in /StarMapDB/index.js. args: ", arguments)
-    const title = "StarMapDB"
-    return (<>
-    <Head>
-        <title>{title}</title>
-        <meta name="description" content={title} />
-        <link rel="icon" href="/favicon.ico" />
+export default class StarMapDB extends Component {
+    constructor(props) { //console.debug("in StarMapDB.constructor", arguements)
+        super(props)
 
-        <link rel="stylesheet" type="text/css"
-            href="/datatables/datatables.min.css"/>
-        <script type="text/javascript"
-            src="/datatables/datatables.min.js"></script>
-    </Head>
-    <App/>
-    <Footer/>
-    </>)
+        this.state = {
+            record: null,
+            recordEntityStub: null,
+            tableEntityStub: UI_ENTITY_STUBS[ ENTITY_PATHS.starSystem ]
+        }
+
+        this.setRecord = this.setRecord.bind(this)
+        this.setTableEntityStub = this.setTableEntityStub.bind(this)
+        this.setContentHeight = this.setContentHeight.bind(this)
+    }
+
+    componentDidMount(){
+        $(window).on('resize', this.setContentHeight)
+        $(window).trigger('resize')
+    }
+
+    setRecord(record, entityStub){
+        this.setState( prevState => {
+            const obj = {
+                record: record
+            }
+
+            if( entityStub ){
+                obj.recordEntityStub = entityStub
+            }
+
+            return obj
+        })
+    }
+    setTableEntityStub( entityStub ){
+        this.setState( prevState => {
+            return {
+                tableEntityStub: entityStub
+            }
+        })
+    }
+
+    setContentHeight(){
+        const vh = $(window).height()
+        const navBar = $('nav.navbar').outerHeight()
+
+        $('main').height( Math.floor(vh - navBar) )
+    }
+
+    render(){ //console.debug("in StarMapDB.render", arguements)
+        const title = "StarMapDB"
+
+        return(<>
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content={title} />
+            <link rel="icon" href="/favicon.ico" />
+
+            <link rel="stylesheet" type="text/css"
+                href="/datatables/datatables.min.css"/>
+            <script type="text/javascript"
+                src="/datatables/datatables.min.js"></script>
+        </Head>
+        <Navbar
+            currentEntityStub={ this.state.tableEntityStub }
+            setEntityStub={ this.setTableEntityStub }
+            theme='light'
+        />
+        <main className='navbar-spacer overflow-auto'>
+            <div className="container-fluid h-100">
+                <div className="row h-100">
+                    <ListView
+                        entityStub={ this.state.tableEntityStub }
+                        setRecord={ this.setRecord }
+                        record={ this.state.record }
+                    />
+                    <DetailsView
+                        record={ this.state.record }
+                        entityStub={ this.state.recordEntityStub }
+                        setRecord={ this.setRecord }
+                    />
+                </div>
+            </div>
+        </main>
+        <Footer/>
+        </>);
+    }
 }
